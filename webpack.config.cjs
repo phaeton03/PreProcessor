95b52f196a5e1dc666d3279081a3576a0f1afc74
niko <phaeton03@gmail.com>
2023-01-16 19:32:36 +0300

    dev - корректировки для прохождения build

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,6 +1,7 @@
 const {resolve} = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
     entry: { main: './js/main.mjs' },
@@ -48,6 +49,13 @@ module.exports = {
           }
         ]
     },
+    optimization: {
+        minimizer: [
+            // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+            `...`,
+            new CssMinimizerPlugin(),
+        ],
+   },
     plugins: [
       new HtmlWebpackPlugin({
       template: './index.html'
